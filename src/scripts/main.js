3a40d457173b5b0d66bd35c2ef49533120063335
Belén Albeza <belen.albeza@gmail.com>
2015-10-28 12:59:58 +0100

    scaffolding with yeoman/bespoke

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  theme = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress');
+
+// Bespoke.js
+bespoke.from('article', [
+  theme(),
+  keys(),
+  touch(),
+  bullets('.bullet, .bullets li'),
+  scale(),
+  hash(),
+  progress()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
